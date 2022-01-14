6abea2a6617baa4f68edd0ec97994c10502efa62
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2022-01-14 16:11:10 +0100

    [main] Cleanup used php-cs-fixer rules

--- templates/cs/.php-cs-fixer.dist.php
+++ templates/cs/.php-cs-fixer.dist.php
@@ -6,13 +6,11 @@ $finder = PhpCsFixer\Finder::create();
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2' => true,
-        'array_syntax' => ['syntax' => 'short'],
         '@Symfony' => true,
-        'blank_line_before_statement' => true,
         'concat_space' => ['spacing' => 'one'],
-        'phpdoc_to_comment' => false,
         'declare_strict_types' => true,
+        'declare_equal_normalize' => true,
+        'declare_parentheses' => true,
     ])
     ->setFinder($finder)
 ;
