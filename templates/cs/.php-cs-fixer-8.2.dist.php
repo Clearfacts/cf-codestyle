6eb886700c5ba3f13a5fb8f6ad8e4d3dcda2606c
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2024-01-31 11:19:36 +0100

    Add php 8.2 file

---
+++ templates/cs/.php-cs-fixer-8.2.dist.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create();
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'concat_space' => ['spacing' => 'one'],
+        'php_unit_method_casing' => ['case' => 'camel_case'],
+        'phpdoc_to_comment' => false,
+        'phpdoc_separation' => false,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters', 'match']],
+        'no_unused_imports' => true,
+        'heredoc_indentation' => true,
+        'ternary_to_null_coalescing' => true,
+        'assign_null_coalescing_to_coalesce_equal' => true,
+        'single_line_throw' => false,
+        'use_nullable_type_declaration' => true,
+    ])
+    ->setFinder($finder)
+;
